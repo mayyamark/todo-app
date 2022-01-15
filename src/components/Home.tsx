f58a66351e2154297cb956534b765c8bf26ca3e7
Mayya Markova <mayya.markova@gmail.com>
2022-01-15 15:14:09 +0200

    Refactoring

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,19 +7,29 @@ import { TodoList } from "./TodoList";
 const Home: React.FC = () => {
   const [todos, setTodos] = useState<Array<Todo>>(initialTodos);
 
-  const toggleComplete: ToggleComplete = selectedTodo => {
-    const updatedTodos = todos.map(todo => {
-      if (todo === selectedTodo) {
+  const toggleComplete: ToggleComplete = (selectedTodo: Todo) => {
+    const updatedTodos = todos.map((todo: Todo) => {
+      if (todo.id === selectedTodo.id) {
         return { ...todo, complete: !todo.complete };
       }
       return todo;
     });
+
     setTodos(updatedTodos);
   };
 
-  const addTodo: AddTodo = newTodo => {
-    newTodo.trim() !== "" &&
-      setTodos([...todos, { text: newTodo, complete: false }]);
+  const addTodo: AddTodo = (newTodoText: string) => {
+    if (newTodoText.trim() !== "") {
+      const newTodo = { 
+        id: todos.length + 1, 
+        text: newTodoText, 
+        complete: false 
+      };
+
+      setTodos([newTodo].concat(todos)); // will add it at the beggining of the list
+    }
+      // On a single line:
+      // newTodoText.trim() !== "" && setTodos([...todos, { id: todos.length + 1, text: newTodoText, complete: false }]); // will add it at the end of the list
   };
 
   return (
--- src/initialTodos.ts
+++ src/initialTodos.ts
@@ -2,11 +2,13 @@ import { Todo } from "./types";
 
 export const initialTodos: Array<Todo> = [
   {
+    id: 2,
+    text: "Make app",
+    complete: false
+  },
+  {
+    id: 1,
     text: "Walk the dog",
     complete: true
   },
-  {
-    text: "Make app",
-    complete: false
-  }
 ];
