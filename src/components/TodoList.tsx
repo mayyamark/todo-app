984e513a94515c95aba1139ea6763fe006b7cd0a
Mayya Markova <mayya.markova@gmail.com>
2022-01-15 13:53:18 +0200

    Add base project

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Todo, ToggleComplete } from "../types";
+import { TodoListItem } from "./TodoListItem";
+
+interface IProps {
+  todos: Array<Todo>;
+  toggleComplete: ToggleComplete;
+}
+
+export const TodoList: React.FC<IProps> = ({
+  todos,
+  toggleComplete
+}) => {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <TodoListItem
+          key={todo.text}
+          todo={todo}
+          toggleComplete={toggleComplete}
+        />
+      ))}
+    </ul>
+  );
+};
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface Todo {
+  text: string;
+  complete: boolean;
+};
+
+export type ToggleComplete = (selectedTodo: Todo) => void;
+
+export type AddTodo = (newTodo: string) => void;
