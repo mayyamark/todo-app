eac71dc06b642a8439235ee3834e9fd5cace6131
Mayya Markova <mayya.markova@gmail.com>
2022-01-25 15:20:57 +0200

    Add logic for dialog

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -2,6 +2,9 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import FormControl from "@mui/material/FormControl";
 import OutlinedInput from "@mui/material/OutlinedInput";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
 import { ChangeEvent, FC, KeyboardEvent, useState } from "react";
 import { AddTodo } from "../types";
 
@@ -12,6 +15,7 @@ interface IProps {
 
 export const AddTodoForm: FC<IProps> = ({ isDisabled, addTodo }) => {
   const [newTodo, setNewTodo] = useState<string>("");
+  const [showDialog, setShowDialog] = useState(false);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setNewTodo(e.target.value);
@@ -26,42 +30,66 @@ export const AddTodoForm: FC<IProps> = ({ isDisabled, addTodo }) => {
     setNewTodo("");
   };
 
+  const handleCloseDialog = () => setShowDialog(false);
+
   return (
-    <FormControl
-      disabled={isDisabled}
-      sx={{ 
-        width: "100%",
-        marginBottom: 2 // 16px
-      }}
-    >
-      <Box 
+    <>
+      <FormControl
+        disabled={isDisabled}
         sx={{ 
-          display: "flex",
-          justifyContent: "center"
+          width: "100%",
+          marginBottom: 2 // 16px
         }}
       >
-        <OutlinedInput 
+        <Box 
           sx={{ 
-            width: "100%",
-            marginRight: 1 // 8px
+            display: "flex",
+            justifyContent: "center"
           }}
-          value={newTodo} 
-          placeholder="Enter a todo..." 
-          onChange={handleChange}
-          onKeyPress={(event: KeyboardEvent<HTMLInputElement>) => {
-            if (event.key === 'Enter') {
-              handleAddTodo();
-            }
-          }}
-        />
-        <Button 
-          variant="contained" 
-          onClick={handleAddTodo}
-          disabled={isDisabled}
         >
-          Add Todo
-        </Button>
-      </Box>
-    </FormControl>
+          <OutlinedInput 
+            sx={{ 
+              width: "100%",
+              marginRight: 1 // 8px
+            }}
+            value={newTodo} 
+            placeholder="Enter a todo..." 
+            onChange={handleChange}
+            onKeyPress={(event: KeyboardEvent<HTMLInputElement>) => {
+              if (event.key === 'Enter') {
+                setShowDialog(true);
+              }
+            }}
+          />
+          <Button 
+            variant="contained" 
+            onClick={() => setShowDialog(true)}
+            disabled={isDisabled}
+          >
+            Add Todo
+          </Button>
+        </Box>
+      </FormControl>
+      {showDialog && (
+          <Dialog
+            open={showDialog}
+            onClose={handleCloseDialog}
+          >
+            <DialogTitle>
+              Are you sure you wanna add it?
+            </DialogTitle>
+            <DialogActions>
+              <Button onClick={() => setShowDialog(false)}>Cancel</Button>
+              <Button onClick={() => {
+                setShowDialog(false);
+                handleAddTodo();
+              }} autoFocus>
+                OK
+              </Button>
+            </DialogActions>
+          </Dialog>
+      )
+      }
+    </>
   );
 };
